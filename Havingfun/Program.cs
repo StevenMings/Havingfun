c60ba3a0ee661f393623fc2e8641396a2e2c568a
Steve Mings <stevennmings@gmail.com>
2018-05-14 18:14:53 -0500

    Add project files.

---
+++ Havingfun/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Havingfun
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
