f597d412c96165ad91276d96904ba909b66374cd
Steve Mings <stevennmings@gmail.com>
2018-05-14 19:06:50 -0500

    Testing weird stuff

--- Havingfun/Program.cs
+++ Havingfun/Program.cs
@@ -6,7 +6,26 @@ namespace Havingfun
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("Hello, what is your name? ");
+            var input = Console.ReadLine();
+
+            while(true)
+            {
+                Console.Write(input + ", did you know that Steve Loves you very much? ");
+                var question = Console.ReadLine();
+
+                if (question == "yes")
+                    break;
+
+                else
+                    Console.WriteLine("Youre wrong, please try again!");
+                
+            }
+                
+                
+
+
+
         }
     }
 }
